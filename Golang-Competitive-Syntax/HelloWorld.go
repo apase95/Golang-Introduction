b15e66b88498f27870da5043179643ef5846262d
apase95 <noobyhandsome@gmail.com>
2026-04-25 21:29:54 +0700

    [IMPLEMENT]: Hello World Program

---
+++ Golang-Competitive-Syntax/HelloWorld.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if _, err := os.Stat("TEST.INP"); err == nil {
+		inFile, _ := os.Open("TEST.INP")
+		defer inFile.Close()
+		os.Stdin = inFile
+
+		outFile, _ := os.Create("TEST.OUT")
+		defer outFile.Close()
+		os.Stdout = outFile
+	}
+
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	fmt.Fprintf(writer, "Hello World!\n")
+}
