a97d57ce73f698c7a5f5143937627a3066bff7d7
apase95 <noobyhandsome@gmail.com>
2026-04-28 22:52:14 +0700

    [IMPLEMENT]: Some Programs about Fast-IO

---
+++ Golang-Competitive-Syntax/Fast-IO/Bufio-Reader-Writer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if _, err := os.Stat("TEST.INP"); err == nil {
+		inFile, _ := os.Open("TEST.INP")
+		defer inFile.Close()
+		os.Stdin = inFile
+
+		outFile, _ := os.Create("TEST.OUT")
+		defer outFile.Close()
+		os.Stdout = outFile
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var str string
+	fmt.Fscanln(reader, &str)
+	fmt.Fprintln(writer, "Hello", str)
+}
