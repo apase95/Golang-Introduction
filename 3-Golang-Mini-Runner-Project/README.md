0bc41e7eb3de9358edcca98d03ec0d7d4a5895dc
apase95 <noobyhandsome@gmail.com>
2026-05-16 23:04:11 +0700

    [REMOVE]: 3-Golang-Mini-Runner-Project

--- 3-Golang-Mini-Runner-Project/README.md
+++
@@ -1,1 +0,0 @@
-# Mini Runner Project
--- 3-Golang-Mini-Runner-Project/internal/repository/store.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "golang-mini-runner-project/internal/models"
-
-type JobStore interface {
-	Save(job *models.Job) error
-	GetByID(id uint) (*models.Job, error)
-	Update(job *models.Job) error
-}
